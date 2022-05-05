2725bab7301a6acdb8a96ab8a812b0a394260eaa
David Wheatley <hi@davwheat.dev>
2022-05-05 12:40:38 +0100

    fix: window accessed during build process

--- src/@types/global/index.d.ts
+++ src/@types/global/index.d.ts
@@ -1,3 +1,11 @@
+import type * as Leaflet from 'leaflet'
+
 declare type ValuesOf<T extends readonly unknown[]> = T[number]
 
 // declare var globalVar: mytype
+
+declare global {
+  interface Window {
+    L: typeof Leaflet
+  }
+}
