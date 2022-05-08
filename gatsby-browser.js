0324986c21742c24c819e1f22bfb7b083f176bd6
David Wheatley <hi@davwheat.dev>
2022-05-08 02:04:10 +0100

    feat: add why did you render support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 import './src/styles/main.less'
+
+import React from 'react'
+
+export const onClientEntry = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render')
+    whyDidYouRender(React, {
+      trackAllPureComponents: true,
+    })
+  }
+}
