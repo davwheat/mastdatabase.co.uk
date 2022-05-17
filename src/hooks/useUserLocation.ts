62a509281e891936b0921aae2c8b7ce59dacae91
David Wheatley <hi@davwheat.dev>
2022-05-17 20:55:54 +0100

    feat: add geolocation permission check helper

--- src/hooks/useUserLocation.ts
+++ src/hooks/useUserLocation.ts
@@ -7,6 +7,37 @@ const defaultOptions = {
 }
 
 /**
+ * Checks the status of the geolocation permission asynchronously.
+ *
+ * Returns the permission status: `'denied'`, `'granted'`, or `'prompt'`.
+ *
+ * Includes a fallback for browsers which do not support the Web Permissions API.
+ */
+export async function getGeolocationPermissionStatus(): Promise<'denied' | 'granted' | 'prompt'> {
+  if ('permissions' in navigator) {
+    return (await navigator.permissions.query({ name: 'geolocation' })).state
+  }
+
+  return new Promise(resolve => {
+    navigator.geolocation.getCurrentPosition(
+      // successfully got location
+      () => resolve('granted'),
+      error => {
+        // permission denied
+        if (error.code == error.PERMISSION_DENIED) resolve('denied')
+
+        // some other error, but not one which is related to a denied permission
+        resolve('granted')
+      },
+      {
+        maximumAge: Infinity,
+        timeout: 0,
+      },
+    )
+  })
+}
+
+/**
  * Returns the user's geolocation, if available.
  *
  * Automatically updates as the user moves around using the Geolocation API's `watchPosition` feature.
