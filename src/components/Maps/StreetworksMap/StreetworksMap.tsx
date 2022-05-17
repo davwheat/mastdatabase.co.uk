b5e447909ba2e709a26c613cf1712e6449a3898e
David Wheatley <hi@davwheat.dev>
2022-05-17 20:57:09 +0100

    feat: swap custom map buttons for FABs; add geolocation denied message; fix no geolocation console errors

--- src/components/Maps/StreetworksMap/StreetworksMap.tsx
+++ src/components/Maps/StreetworksMap/StreetworksMap.tsx
@@ -1,11 +1,11 @@
-import React, { useMemo, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 import useFixLeafletAssets from '@hooks/useFixLeafletAssets'
-import { useUserLocation } from '@hooks/useUserLocation'
+import { getGeolocationPermissionStatus, useUserLocation } from '@hooks/useUserLocation'
 import useForceRender from '@hooks/useForceRerender'
 
 import { MapContainer, ScaleControl, useMap, useMapEvent } from 'react-leaflet'
-import { makeStyles } from '@material-ui/core'
+import { Fab, makeStyles, Zoom } from '@material-ui/core'
 
 import clsx from 'clsx'
 import dayjs from 'dayjs'
@@ -69,22 +69,16 @@ const useStyles = makeStyles({
     gap: 16,
   },
   customButton: {
-    all: 'unset',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    boxSizing: 'border-box',
-    borderRadius: '50%',
-    padding: 8,
-    background: '#fff',
     height: 52,
     width: 52,
-
-    cursor: 'pointer',
 
     '& svg': {
       display: 'block',
       margin: 'auto',
+    },
+
+    '&:disabled': {
+      cursor: 'not-allowed',
     },
   },
   geolocationButton: {
@@ -105,46 +99,59 @@ function CustomControlButtons() {
 
   const [settingsDialogOpen, setSettingsDialogOpen] = useState(false)
 
-  const location: [number, number] = geolocation ? [geolocation.latitude, geolocation.longitude] : null
-  const isMapLocationCentred = map.getCenter().equals(location, 0.00001)
+  const location: [number, number] | null = geolocation ? [geolocation.latitude, geolocation.longitude] : null
+  const isMapLocationCentred = location ? map.getCenter().equals(location, 0.00001) : false
 
   useMapEvent(
     'move',
-    useMemo(
-      () => () => {
-        const nowCentred = map.getCenter().equals(location, 0.00001)
+    useCallback(() => {
+      if (!location) return
 
-        if (nowCentred !== isMapLocationCentred) forceRerender()
-      },
-      [location?.[0], location?.[1], map, forceRerender, isMapLocationCentred],
-    ),
+      const nowCentred = map.getCenter().equals(location, 0.00001)
+
+      if (nowCentred !== isMapLocationCentred) forceRerender()
+    }, [location?.[0], location?.[1], map, forceRerender, isMapLocationCentred]),
   )
+
+  useMapEvent('enterFullscreen', forceRerender)
+  useMapEvent('exitFullscreen', forceRerender)
 
   return (
     <>
       <div className={classes.customButtonsContainer}>
-        <button
-          className={clsx(classes.customButton)}
-          aria-label="Map settings"
-          onClick={() => {
-            setSettingsDialogOpen(true)
-          }}
-          data-located={isMapLocationCentred}
-        >
-          <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z"
-            />
-          </svg>
-        </button>
+        <Zoom in={!map.isFullscreen}>
+          <Fab
+            className={clsx(classes.customButton)}
+            aria-label="Map settings"
+            onClick={() => {
+              setSettingsDialogOpen(true)
+            }}
+            data-located={isMapLocationCentred}
+          >
+            <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z"
+              />
+            </svg>
+          </Fab>
+        </Zoom>
 
-        <button
+        <Fab
+          color={isMapLocationCentred ? 'primary' : 'default'}
           className={clsx(classes.customButton, classes.geolocationButton)}
           aria-label="Pan map to current location"
-          onClick={() => {
+          onClick={useCallback(async () => {
+            if (!location) {
+              if ((await getGeolocationPermissionStatus()) === 'denied') {
+                alert(`You have denied access to your location. Please enable it in your browser settings.`)
+              }
+
+              return
+            }
+
             map.flyTo(location, 14)
-          }}
+          }, [location, getGeolocationPermissionStatus])}
           data-located={isMapLocationCentred}
         >
           <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
@@ -153,7 +160,7 @@ function CustomControlButtons() {
               d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M3.05,13H1V11H3.05C3.5,6.83 6.83,3.5 11,3.05V1H13V3.05C17.17,3.5 20.5,6.83 20.95,11H23V13H20.95C20.5,17.17 17.17,20.5 13,20.95V23H11V20.95C6.83,20.5 3.5,17.17 3.05,13M12,5A7,7 0 0,0 5,12A7,7 0 0,0 12,19A7,7 0 0,0 19,12A7,7 0 0,0 12,5Z"
             />
           </svg>
-        </button>
+        </Fab>
       </div>
 
       <PromoterSettingsDialog open={settingsDialogOpen} onClose={() => setSettingsDialogOpen(false)} />
