ea1749b217e1fa063ffddd9e998c9c9f7624be3f
David Wheatley <hi@davwheat.dev>
2022-07-16 08:12:44 +0100

    wip

--- src/data/SpectrumAllocation/index.ts
+++
@@ -1,5 +0,0 @@
-import { ISpectrumAllocation } from '@components/MobileNetworking/SpectrumMap'
-
-export function mapBandAndData(number: number | string | number[] | string[], data: ISpectrumAllocation[]) {
-  return { band: number, data }
-}
