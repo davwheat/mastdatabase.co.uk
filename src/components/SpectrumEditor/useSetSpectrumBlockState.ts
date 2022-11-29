937a79688df117031e0349abab863d47acc05af2
David Wheatley <david@davwheat.dev>
2022-11-29 16:47:51 +0000

    fix: add option to delete a spectrum block

--- src/components/SpectrumEditor/useSetSpectrumBlockState.ts
+++ src/components/SpectrumEditor/useSetSpectrumBlockState.ts
@@ -7,16 +7,23 @@ import jsonStableStringify from 'json-stable-stringify-without-jsonify'
 export function useSetSpectrumBlockState(dataIndex: number, blockIndex: number) {
   const setSpectrumEditorState = useSetRecoilState(SpectrumEditorAtom)
 
-  return (block: SpectrumBlock | ((block: SpectrumBlock) => SpectrumBlock)) => {
+  return (block: SpectrumBlock | null | ((block: SpectrumBlock) => SpectrumBlock | null)) => {
     setSpectrumEditorState(state => {
+      debugger
+
       const newState = { ...state }
 
       const originalEditorState = parseStringAsSpectrumData(newState.rawInput)
 
       if (!originalEditorState) throw Error('Invalid existing state -- bailing out for update')
 
-      originalEditorState[dataIndex].spectrumData[blockIndex] =
-        typeof block === 'function' ? block(originalEditorState[dataIndex].spectrumData[blockIndex]) : block
+      const newData = typeof block === 'function' ? block(originalEditorState[dataIndex].spectrumData[blockIndex]) : block
+
+      if (newData === null) {
+        originalEditorState[dataIndex].spectrumData.splice(blockIndex, 1)
+      } else {
+        originalEditorState[dataIndex].spectrumData[blockIndex] = newData
+      }
 
       newState.rawInput = jsonStableStringify(originalEditorState, { space: 2 })
 
