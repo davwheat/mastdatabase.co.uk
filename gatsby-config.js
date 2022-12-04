18fdfc72c42b126c2104bedca821d12533b408de
David Wheatley <david@davwheat.dev>
2022-12-04 21:33:25 +0000

    feat: add dev TS improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ if (__IS_DEV__) {
 module.exports = {
   flags: {
     // FAST_DEV: true,
+    // DEV_SSR: true,
   },
   siteMetadata: {
     title: `Mast Database`,
@@ -34,6 +35,10 @@ module.exports = {
     author: `@davwheat`,
     siteUrl: `https://mastdatabase.co.uk`,
   },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
   plugins: [
     ...prodPlugins,
     {
