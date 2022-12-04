17bb299237c303cf2d6ee24c3eb01ba1f17f5297
David Wheatley <david@davwheat.dev>
2022-12-04 17:25:18 +0000

    feat: add north macedonian spectrum pages

---
+++ src/pages/mk/index.tsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+import Section from '@components/Design/Section'
+import Hero from '@components/Design/Hero'
+import Layout from '@components/Layout'
+import CardLink from '@components/MobileNetworking/CardLink'
+import Breadcrumbs from '@components/Design/Breadcrumbs'
+
+import Colors from '@data/colors.json'
+import Breakpoints from '@data/breakpoints'
+
+import { makeStyles } from '@material-ui/styles'
+
+import type { PageProps } from 'gatsby'
+
+interface ILinkGroup {
+  groupName: string
+  groupDescription: string
+  groupLinks: ILink[]
+}
+
+interface ILink {
+  title: string
+  description: string
+  url: string
+}
+
+const Links: ILinkGroup[] = [
+  {
+    groupName: 'Datasets',
+    groupDescription: 'A variety of North Macedonian mobile networking related datasets compiled by the community, available for free.',
+    groupLinks: [
+      {
+        title: 'NR/EARFCN list',
+        description: 'A list of North Macedonian mobile network frequencies by their NRARFCNs and EARFCNs.',
+        url: '/mk/arfcn-list',
+      },
+      {
+        title: 'Spectrum allocation',
+        description: 'A visualisation of spectrum allocation for mobile networks within North Macedonia.',
+        url: '/mk/spectrum',
+      },
+    ],
+  },
+]
+
+const useStyles = makeStyles({
+  linkList: {
+    marginTop: 24,
+    display: 'grid',
+    gridTemplateColumns: 'repeat(var(--columns), minmax(0, 1fr))',
+    gap: 32,
+    '--columns': 1,
+
+    [Breakpoints.downTo.tablet]: {
+      '--columns': 2,
+    },
+  },
+})
+
+export default function MobileNetworkingPage({ location }: PageProps) {
+  const classes = useStyles()
+
+  return (
+    <Layout
+      location={location}
+      title="Mobile networking in North Macedonia"
+      description="A collection of North Macedonian mobile networking tools and datasets compiled by the community."
+    >
+      <Hero firstElement size="huge" color={Colors.primaryBlue}>
+        <h1 className="text-shout">Mobile networking in North Macedonia</h1>
+        <p role="doc-subtitle" className="text-loud">
+          A collection of North Macedonian mobile networking tools and datasets compiled by the community.
+        </p>
+      </Hero>
+
+      <Breadcrumbs
+        data={[
+          { t: 'Home', url: '/' },
+          { t: 'North Macedonia', url: '/mk' },
+        ]}
+      />
+
+      {Links.map((group, i) => (
+        <Section key={i} darker={i % 2 === 0 ? false : true} width="wider">
+          <h2 className="text-louder">{group.groupName}</h2>
+          <p className="text-speak">{group.groupDescription}</p>
+
+          <div className={classes.linkList}>
+            {group.groupLinks.map(link => (
+              <CardLink key={link.title} {...link} />
+            ))}
+          </div>
+        </Section>
+      ))}
+    </Layout>
+  )
+}
