e87b54816467a8b4deb335f558f7610f4e2afeab
David Wheatley <david@davwheat.dev>
2022-12-10 01:52:40 +0000

    perf: use self-hosted tile server for 3 UK coverage map

--- src/components/Maps/UkCoverageMap/Providers/3UKProvider.tsx
+++ src/components/Maps/UkCoverageMap/Providers/3UKProvider.tsx
@@ -1,12 +1,47 @@
 import React from 'react'
-
-import FunctionalTileLayer from '@leaflet/FunctionalTileLayer'
 import CoverageProvider, { ICoverageLayer, ICoverageLayerKey } from './CoverageProvider'
+
+import dayjs from 'dayjs'
+import { TileLayer } from 'react-leaflet'
 
 export default class ThreeUkCoverageMapProvider extends CoverageProvider {
   providerName: string = 'Three UK'
   defaultLayerId: number = 2
   supportsSites: boolean = false
+
+  protected readonly version: string = '2022-11-15'
+
+  private readonly zoomLevels: Record<number, [number, number]> = {
+    14: [12, Infinity],
+    12: [9, 11.999],
+    9: [7, 8.999],
+  }
+
+  protected getTileUrl(layer: string): string {
+    return `https://234-20.coveragetiles.com/${this.version}/${layer}/{z}/{x}/{y}.png`
+  }
+
+  protected getTileLayerEntry(label: string, layer: string): ICoverageLayer {
+    return {
+      label,
+      layers: (
+        <>
+          {Object.entries(this.zoomLevels).map(([nativeZoom, [minZoom, maxZoom]]) => (
+            <TileLayer
+              minNativeZoom={parseInt(nativeZoom)}
+              maxNativeZoom={parseInt(nativeZoom)}
+              minZoom={minZoom}
+              maxZoom={maxZoom}
+              key={this.getTileUrl(layer)}
+              opacity={0.5}
+              url={this.getTileUrl(layer)}
+              attribution={this.attributionTemplate(label)}
+            />
+          ))}
+        </>
+      ),
+    }
+  }
 
   getLayerKeys(): ICoverageLayerKey[] {
     return [
@@ -38,103 +73,15 @@ export default class ThreeUkCoverageMapProvider extends CoverageProvider {
   }
 
   getPageMessages(): string[] {
-    return [
-      'Three UK implement rate-limiting on their coverage map API. If coloured tiles stop loading, please wait a few minutes, then move the map again. You are less likely to encounter this issue when you are zoomed in closely.',
-    ]
-  }
-
-  private readonly bounds: Record<number, [number, number, number, number]> = {
-    14: [7765, 8335, 10810, 11718],
-    12: [1940, 2083, 2702, 2929],
-    9: [242, 259, 337, 366],
-  }
-
-  private readonly zoomLevels: Record<number, [number, number, number]> = {
-    14: [12, 14, Infinity],
-    12: [9, 12, 11.999],
-    9: [7, 9, 8.999],
-  }
-
-  private makeFunctionalTileUrl(nativeZoom: number, layerName: string) {
-    const [minZoom, tileZoomLevel, maxZoom] = this.zoomLevels[nativeZoom]
-    const [x1, x2, i1, i2] = this.bounds[nativeZoom]
-
-    return (
-      <FunctionalTileLayer
-        key={`${layerName}_${tileZoomLevel}`}
-        maxNativeZoom={tileZoomLevel}
-        minNativeZoom={tileZoomLevel}
-        maxZoom={maxZoom}
-        minZoom={minZoom}
-        opacity={0.5}
-        tileSize={256}
-        tileFunction={view => {
-          let { column: x, row: y } = view.tile
-
-          const i = (1 << tileZoomLevel) - y - 1
-
-          if (x >= x1 && x <= x2 && i >= i1 && i <= i2) {
-            // const qs = new URLSearchParams({
-            //   layer: layerName,
-            //   zoom: tileZoomLevel.toString(),
-            //   x: x.toString(),
-            //   i: i.toString(),
-            // })
-
-            // return `https://proxies.mastdatabase.co.uk/uk/three/coverage-map/tiles?${qs.toString()}`
-
-            return `https://www.three.co.uk/static/images/functional_apps/coverage/${layerName}/${tileZoomLevel}/${x}/${i}.png`
-          } else {
-            // blank image
-            return `data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==`
-          }
-        }}
-      />
-    )
+    return [`Coverage tiles last updated ${dayjs(this.version).format('D MMMM YYYY')}.`]
   }
 
   getLayers(): ICoverageLayer[] {
     return [
-      {
-        label: '3G',
-        layers: (
-          <>
-            {this.makeFunctionalTileUrl(14, 'Fast')}
-            {this.makeFunctionalTileUrl(12, 'Fast')}
-            {this.makeFunctionalTileUrl(9, 'Fast')}
-          </>
-        ),
-      },
-      {
-        label: '4G',
-        layers: (
-          <>
-            {this.makeFunctionalTileUrl(14, 'LTE')}
-            {this.makeFunctionalTileUrl(12, 'LTE')}
-            {this.makeFunctionalTileUrl(9, 'LTE')}
-          </>
-        ),
-      },
-      {
-        label: '4G (incl. 800 MHz)',
-        layers: (
-          <>
-            {this.makeFunctionalTileUrl(14, '800')}
-            {this.makeFunctionalTileUrl(12, '800')}
-            {this.makeFunctionalTileUrl(9, '800')}
-          </>
-        ),
-      },
-      {
-        label: '5G',
-        layers: (
-          <>
-            {this.makeFunctionalTileUrl(14, 'FiveG')}
-            {this.makeFunctionalTileUrl(12, 'FiveG')}
-            {this.makeFunctionalTileUrl(9, 'FiveG')}
-          </>
-        ),
-      },
+      this.getTileLayerEntry('3G', '3g'),
+      this.getTileLayerEntry('4G', '4g'),
+      this.getTileLayerEntry('4G VoLTE', '4g_volte'),
+      this.getTileLayerEntry('5G', '5g'),
     ]
   }
 }
