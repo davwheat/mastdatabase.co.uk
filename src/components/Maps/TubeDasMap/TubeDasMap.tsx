c0b21fbb39f6060764cdd1bd5ea6bca411c5840d
David Wheatley <david@davwheat.dev>
2022-12-29 03:53:07 +0000

    feat: add basic tube mobile connectivity map

---
+++ src/components/Maps/TubeDasMap/TubeDasMap.tsx
@@ -0,0 +1,267 @@
+import React, { useEffect, useCallback } from 'react'
+
+import { AttributionControl, MapContainer, TileLayer, useMap, GeoJSON, Pane } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+import TflLines from './MapData/tfl_lines.geo.json'
+import TflStations from './MapData/tfl_stations.geo.json'
+import TflZones from './MapData/tfl_zones_1_to_6.geo.json'
+import Thames from './MapData/thames.geo.json'
+
+import useFixLeafletAssets from '@hooks/useFixLeafletAssets'
+import GeolocationButton from '@leaflet/GeolocationButton'
+import MapCustomButtonsContainer from '@leaflet/MapCustomButtonsContainer'
+
+import type { PathOptions, Layer } from 'leaflet'
+import type * as geojson from 'geojson'
+
+const MULTI_LINE_STROKE_COLOUR_ALTERNATE = 8
+const LINE_WIDTH = 5
+
+function styleZoneData(feature: geojson.Feature<geojson.GeometryObject, any> | undefined): PathOptions {
+  const zone: string = feature?.properties?.name ?? ''
+
+  if (!zone) return {}
+
+  const zoneNumber = parseInt(zone.substring(5))
+
+  const commonOptions = {
+    stroke: false,
+    fill: true,
+  }
+
+  switch (zoneNumber) {
+    default:
+    case 1:
+    case 3:
+    case 5:
+      return {
+        ...commonOptions,
+        fillColor: 'rgba(255, 255, 255, 0.0)',
+      }
+
+    case 2:
+    case 4:
+      return {
+        ...commonOptions,
+        fillColor: 'rgba(0, 0, 0, 0.35)',
+      }
+
+    case 6:
+      return {
+        ...commonOptions,
+        fillColor: 'rgba(0, 0, 0, 0.45)',
+      }
+  }
+}
+
+function styleThamesData(feature: geojson.Feature<geojson.GeometryObject, any> | undefined): PathOptions {
+  return {
+    stroke: false,
+    fill: true,
+    fillColor: '#80becd',
+    fillOpacity: 0.6,
+  }
+}
+
+function getLinesFromFeature(feature: geojson.Feature<geojson.GeometryObject, any> | undefined): { name: string }[] {
+  let lines = feature?.properties?.lines
+
+  const BLACKLISTED_LINES = ['National Rail', 'Tramlink', 'IFS Cloud Cable Car', 'London Overground']
+
+  if (Array.isArray(lines)) {
+    lines = lines.filter(line => {
+      // Only active lines
+      if (!((line?.opened ?? 0) <= 2022 && (line?.closed ?? 9999) >= 2022)) return false
+
+      if (BLACKLISTED_LINES.includes(line?.name ?? '')) return false
+
+      return true
+    })
+  }
+
+  return lines ?? []
+}
+
+function filterLineData(feature: geojson.Feature<geojson.GeometryObject, any> | undefined): boolean {
+  const lines = getLinesFromFeature(feature)
+
+  if (!lines) return true
+  if (lines.length === 0) return false
+
+  return true
+}
+
+const lineAttrs = {
+  Bakerloo: { id: 'B', colour: '#B36305', network: 'Tube' },
+  Central: { id: 'C', colour: '#E32017', network: 'Tube' },
+  Circle: { id: 'I', colour: '#FFD300', network: 'Tube' },
+  'Elizabeth line': { id: 'X', colour: '#7156A5', network: 'Rail' },
+  DLR: { id: 'L', colour: '#00A4A7', network: 'DLR' },
+  District: { id: 'D', colour: '#00782A', network: 'Tube' },
+  'IFS Cloud Cable Car': { id: 'A', colour: '#E51836', network: 'IFS Cloud Cable Car' },
+  'Hammersmith & City': { id: 'H', colour: '#F3A9BB', network: 'Tube' },
+  Jubilee: { id: 'J', colour: '#A0A5A9', network: 'Tube' },
+  Metropolitan: { id: 'M', colour: '#9B0056', network: 'Tube' },
+  Northern: { id: 'N', colour: '#000000', network: 'Tube' },
+  'London Overground': { id: 'O', colour: '#EE7C0E', network: 'Rail' },
+  Piccadilly: { id: 'P', colour: '#003688', network: 'Tube' },
+  Tramlink: { id: 'T', colour: '#84B817', network: 'Tramlink' },
+  Victoria: { id: 'V', colour: '#0098D4', network: 'Tube' },
+  'Waterloo & City': { id: 'W', colour: '#95CDBA', network: 'Tube' },
+}
+
+function styleLineData(feature: geojson.Feature<geojson.GeometryObject, any> | undefined): PathOptions {
+  const lines = getLinesFromFeature(feature).map(l => l.name)
+
+  const firstLineColor = lineAttrs[lines[0]]?.colour ?? '#000'
+
+  return {
+    weight: LINE_WIDTH,
+    color: firstLineColor,
+    fill: true,
+    fillColor: '#fff',
+    lineCap: 'butt',
+  }
+}
+
+function stationMarker(geoJsonPoint, latLng): Layer {
+  const L = window.L as typeof import('leaflet')
+
+  const lines = getLinesFromFeature(geoJsonPoint)
+
+  const popupContent = document.createElement('div')
+
+  popupContent.innerHTML = `
+  <p class="text-whisper-loud">${geoJsonPoint.properties.name}</p>
+  <p class="text-whisper">Served by: ${lines.map(l => l?.name ?? '???').join(', ')}</p>
+`
+
+  return new L.CircleMarker(latLng, {
+    radius: 6,
+    fillColor: '#fff',
+    fillOpacity: 1,
+    color: lines.length === 1 ? lineAttrs[lines[0].name].colour : '#000',
+    weight: 2.5,
+  }).bindPopup(popupContent)
+}
+
+export default function TubeDasMap() {
+  useFixLeafletAssets()
+
+  return (
+    <MapContainer
+      style={{
+        height: '60vh',
+        backgroundColor: '#fff',
+      }}
+      center={[51.509865, -0.118092]}
+      zoom={11}
+      minZoom={1}
+      attributionControl={false}
+    >
+      <MapLayers />
+
+      <MapComponents />
+
+      <AttributionControl position="bottomright" prefix={undefined} />
+
+      <MapCustomButtonsContainer>
+        <GeolocationButton />
+      </MapCustomButtonsContainer>
+    </MapContainer>
+  )
+}
+
+function MapLayers() {
+  const L = window.L as typeof import('leaflet')
+
+  const map = useMap()
+
+  const onLineLayerMade = useCallback(
+    (feature, layer) => {
+      const L = window.L as typeof import('leaflet')
+
+      const lines = getLinesFromFeature(feature).map(l => l.name)
+
+      if (lines.length <= 1) return
+
+      const allLineColours = lines.map(l => lineAttrs[l]?.colour ?? '#000')
+      allLineColours.splice(0, 1)
+
+      const coords = feature.geometry.coordinates
+      const latLngs = L.GeoJSON.coordsToLatLngs(coords)
+
+      allLineColours.forEach((color, i) => {
+        const extraNum = i + 1
+        const totalCount = lines.length
+
+        let dashArray = `${MULTI_LINE_STROKE_COLOUR_ALTERNATE} ${MULTI_LINE_STROKE_COLOUR_ALTERNATE * (totalCount - 1)}`
+
+        const popupContent = document.createElement('div')
+
+        popupContent.innerHTML = `
+  <p class="text-whisper"><strong>Track segment ID:</strong> ${feature.properties.id}</p>
+`
+
+        new L.Polyline(latLngs, {
+          dashArray,
+          dashOffset: (MULTI_LINE_STROKE_COLOUR_ALTERNATE * extraNum).toString(),
+          color,
+          lineCap: 'butt',
+          weight: LINE_WIDTH,
+        })
+          .bindPopup(popupContent)
+          .addTo(map)
+      })
+    },
+    [map],
+  )
+
+  return (
+    <>
+      <Pane name="stations" style={{ zIndex: 100 }}>
+        <GeoJSON data={TflStations as any} filter={filterLineData} pointToLayer={stationMarker} />
+      </Pane>
+
+      <Pane name="lines" style={{ zIndex: 50 }}>
+        <GeoJSON data={TflLines as any} filter={filterLineData} style={styleLineData} onEachFeature={onLineLayerMade} />
+      </Pane>
+
+      <Pane name="zones" style={{ zIndex: 10 }}>
+        <GeoJSON data={TflZones as any} style={styleZoneData} />
+      </Pane>
+
+      <Pane name="thames" style={{ zIndex: 1 }}>
+        <GeoJSON data={Thames as any} style={styleThamesData} />
+      </Pane>
+
+      <Pane name="base" style={{ zIndex: 0 }}>
+        <TileLayer
+          opacity={0.45}
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Pane>
+    </>
+  )
+}
+
+function MapComponents() {
+  const L = window.L as typeof import('leaflet')
+
+  const map = useMap()
+
+  useEffect(() => {
+    if (!map.fullScreenControl) {
+      L.fullScreen({
+        position: 'topleft',
+        title: 'Enter fullscreen mode',
+        titleCancel: 'Exit fullscreen mode',
+        forceSeparateButton: true,
+      }).addTo(map)
+    }
+  })
+
+  return null
+}
---
+++ src/pages/gb/london-underground-connectivity.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+import Layout from '@components/Layout'
+import Hero from '@components/Design/Hero'
+import Breadcrumbs from '@components/Design/Breadcrumbs'
+import Section from '@components/Design/Section'
+
+import Colors from '@data/colors.json'
+
+import { makeStyles, NoSsr } from '@material-ui/core'
+
+import StreetworksMap from '@components/Maps/StreetworksMap/StreetworksMap'
+
+import type { PageProps } from 'gatsby'
+import { ErrorBoundaryContext } from 'react-use-error-boundary'
+import { StreetworksMapSettings } from '@components/Maps/StreetworksMap/StreetworksMapSettings'
+import Link from '@components/Links/Link'
+import TubeDasMap from '@components/Maps/TubeDasMap/TubeDasMap'
+
+const useStyles = makeStyles({
+  mapSection: {
+    '& .leaflet-popup-content': {
+      '& dt': {
+        fontWeight: 'bold',
+
+        '&:not(:first-child)': {
+          marginTop: 8,
+        },
+      },
+    },
+  },
+})
+
+export default function StreetworksMapPage({ location }: PageProps) {
+  const classes = useStyles()
+
+  return (
+    <Layout
+      location={location}
+      title="UK telecoms streetworks map"
+      description="See active and future telecoms streetworks across the United Kingdom."
+    >
+      <ErrorBoundaryContext>
+        <Hero firstElement size="huge" color={Colors.primaryRed}>
+          <h1 className="text-shout">London Underground connectivity</h1>
+          <p role="doc-subtitle" className="text-loud">
+            Interactive map of mobile network deployments on the London Underground.
+          </p>
+        </Hero>
+
+        <Breadcrumbs
+          data={[
+            { t: 'Home', url: '/' },
+            { t: 'Maps', url: '/maps' },
+            { t: 'London Underground connectivity', url: '/maps/london-underground-connectivity' },
+          ]}
+        />
+
+        <Section>
+          <h2 className="text-loud">Mobile connectivity on the Tube</h2>
+          <p className="text-speak">
+            Mobile connectivity on the London Underground is provided by a combination of leaky feeders in tunnels and DAS units in stations. The
+            programme is operated by{' '}
+            <Link href="https://www.baicommunications.com/mediarelease/bai-communications-awarded-20-year-concession-to-deliver-high-speed-mobile-coverage-across-the-london-underground/">
+              BAI Communications as part of a 20-year concession awarded by Transport for&nbsp;London
+            </Link>
+            .
+          </p>
+          <p className="text-speak">
+            The programme entails full 4G connectivity throughout the London Underground by the end of 2024, as well as 5G connectivity at some
+            select stations. As of now, the deployment is not yet complete, hence a map of deployments and other information is useful for those
+            interested in tracking the rollout over&nbsp;time.
+          </p>
+        </Section>
+
+        <Section width="full" className={classes.mapSection}>
+          <NoSsr>
+            <TubeDasMap />
+          </NoSsr>
+        </Section>
+      </ErrorBoundaryContext>
+    </Layout>
+  )
+}
