1e31a28f7b25e9402703f5a88c6a49d0ad53ecff
David Wheatley <david@davwheat.dev>
2023-01-20 17:14:15 +0000

    feat: incorporate new estonian spectrum data
    https://github.com/davwheat/mobile-spectrum-data/issues/15
    https://github.com/davwheat/mobile-spectrum-data/pull/16

---
+++ src/pages/ee/spectrum.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import Hero from '@components/Design/Hero'
+import Layout from '@components/Layout'
+import Breadcrumbs from '@components/Design/Breadcrumbs'
+import AllSpectrumMaps from '@components/MobileNetworking/AllSpectrumMaps'
+
+import Colors from '@data/colors.json'
+import EEAllBands from 'mobile-spectrum-data/EE'
+import { PageProps } from 'gatsby'
+import SpectrumTotaller from '@components/MobileNetworking/SpectrumTotaller'
+
+function EESpectrumAllocationPage({ location }: PageProps) {
+  return (
+    <Layout
+      location={location}
+      title="Estonian mobile spectrum allocation"
+      description="A visualisation of spectrum allocation across Estonia, per operator and radio access technology."
+    >
+      <Hero firstElement size="huge" color={Colors.primaryBlue}>
+        <h1 className="text-shout">Estonian mobile spectrum allocation</h1>
+        <p role="doc-subtitle" className="text-loud">
+          A visualisation of spectrum allocation across Estonia, per operator and radio access technology.
+        </p>
+      </Hero>
+
+      <Breadcrumbs
+        data={[
+          { t: 'Home', url: '/' },
+          { t: 'Estonia', url: '/ee' },
+          { t: 'Mobile spectrum allocation', url: '/ee/spectrum' },
+        ]}
+      />
+
+      <SpectrumTotaller countryCode="EE" bandsData={EEAllBands} />
+
+      <AllSpectrumMaps locationName="Estonia" countryCode="EE" bandsData={EEAllBands} />
+    </Layout>
+  )
+}
+
+export default EESpectrumAllocationPage
