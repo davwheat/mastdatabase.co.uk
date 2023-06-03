9c9cd4d2aa7ac73b3006cfd6a1c3bcc3292b86fa
David Wheatley <david@davwheat.dev>
2023-06-03 23:34:50 +0100

    fix: extra lines showing with connected only filter on

--- src/components/Maps/TubeDasMap/TubeDasMap.tsx
+++ src/components/Maps/TubeDasMap/TubeDasMap.tsx
@@ -126,7 +126,9 @@ const lineAttrs = {
   'Waterloo & City': { id: 'W', colour: '#95CDBA', network: 'Tube' },
 }
 
-function styleCoveredLineData(feature: geojson.Feature<geojson.GeometryObject, GeoJsonLineProperties>): PathOptions {
+function styleCoveredLineData(feature: geojson.Feature<geojson.GeometryObject, GeoJsonLineProperties> | undefined): PathOptions {
+  if (!feature) return {}
+
   const segments = getStationSegmentsFromLineData(feature)
   const hasConnectivity = segments && isLineSegmentCovered(...segments)
 
@@ -372,8 +374,9 @@ function MapLayers({ hideSectionsWithNoConnectivity, hiddenLines }: TubeDasMapPr
 
           if (hasConnectivity === 'none') return false
         } else {
+          // Line
           const stationSegments = getStationSegmentsFromLineData(feature)
-          const hasConnectivity = stationSegments && isLineSegmentCovered(...stationSegments)
+          const hasConnectivity = (stationSegments && isLineSegmentCovered(...stationSegments)) ?? 'none'
 
           if (hasConnectivity === 'none') return false
         }
@@ -394,7 +397,7 @@ function MapLayers({ hideSectionsWithNoConnectivity, hiddenLines }: TubeDasMapPr
       const stationSegments = getStationSegmentsFromLineData(feature!)
       if (!stationSegments) return false
 
-      const hasConnectivity = isLineSegmentCovered(...stationSegments)
+      const hasConnectivity = isLineSegmentCovered(...stationSegments) ?? 'none'
 
       return hasConnectivity !== 'none'
     },
