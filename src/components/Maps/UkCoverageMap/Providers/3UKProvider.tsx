106cb0dd9fe18a7247b924b61eca13368a50641a
David Wheatley <david@davwheat.dev>
2023-07-13 09:40:01 +0100

    chore: bump 3 UK map to July 2023
    Signed-off-by: David Wheatley <david@davwheat.dev>

--- src/components/Maps/UkCoverageMap/Providers/3UKProvider.tsx
+++ src/components/Maps/UkCoverageMap/Providers/3UKProvider.tsx
@@ -17,9 +17,10 @@ export default class ThreeUkCoverageMapProvider extends CoverageProvider<true> {
     '2023-01-26': '26 Jan 2023',
     '2023-03-24': '24 Mar 2023',
     '2023-05-12': '12 May 2023',
+    '2023-07-12': '12 July 2023',
   }
 
-  protected version: string = '2023-05-12'
+  protected version: string = '2023-07-12'
 
   protected getTileUrl(layer: string, version: string = this.version): string {
     return `https://234-20.coveragetiles.com/${version}/${layer}/{z}/{x}/{y}.png`
