01aedc1375122b80b685f3c3f0fbdd0db76fa531
David Wheatley <david@davwheat.dev>
2024-04-10 12:00:55 +0200

    chore: vf uk map 5 april 2024

--- src/components/Maps/UkCoverageMap/Providers/VFProvider.tsx
+++ src/components/Maps/UkCoverageMap/Providers/VFProvider.tsx
@@ -31,8 +31,9 @@ export default class VodafoneCoverageMapProvider extends CoverageProvider<true> 
     '2024-03-24': '24 March 2024',
     '2024-03-27': '27 March 2024',
     '2024-03-28': '28 March 2024',
+    '2024-04-05': '5 April 2024',
   }
-  protected _version = '2024-03-28'
+  protected _version = '2024-04-05'
 
   protected _getLayerKeys(version: string): ICoverageLayerKey[] {
     const genericKey = [
