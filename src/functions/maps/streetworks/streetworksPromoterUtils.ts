7aadb895d24a3f4c9c89c6a8dd29a5e0bdb991fd
Joshua <53623449+Joshua-Noakes1@users.noreply.github.com>
2025-12-17 22:09:54 +0000

    Update OFNL and Truespeed
    Add OFNL 'Open Fibre Networks Ltd' as seen on one network in Abberton (UX007OFNLN0025199ESW-01) and add County Broadband to Truespeed following them being acquired https://countybroadband.co.uk/merger/

--- src/functions/maps/streetworks/streetworksPromoterUtils.ts
+++ src/functions/maps/streetworks/streetworksPromoterUtils.ts
@@ -162,7 +162,7 @@ export const AllStreetworksPromoters: IOneNetworkStreetworksPromoter[] = [
   {
     id: 'ofnl',
     name: 'Open Fibre Networks Limited',
-    aliases: ['Open Fibre Networks Limited', 'Open Fibre Networks', 'Independent Fibre Networks'],
+    aliases: ['Open Fibre Networks Limited', 'Open Fibre Networks', 'Independent Fibre Networks', 'Open Fibre Networks Ltd'],
     category: 'Fixed broadband',
     icon: {
       text: 'OFNL',
@@ -412,7 +412,7 @@ export const AllStreetworksPromoters: IOneNetworkStreetworksPromoter[] = [
   {
     id: 'truespeed',
     name: 'Truespeed',
-    aliases: ['Truespeed', 'Truespeed Communications', 'Truespeed Communications Ltd'],
+    aliases: ['Truespeed', 'Truespeed Communications', 'Truespeed Communications Ltd', 'County Broadband LTD'],
     category: 'Fixed broadband',
     icon: {
       text: 'TRUE',
